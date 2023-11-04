60066468202115fdefaf5cc7e0bfe153c8ca67f7
Crowdbotics <team@crowdbotics.com>
2023-11-04 18:56:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-king-44338",
+  url: "https://shiny-king-44338.botics.co",
+  api: "https://shiny-king-44338.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
